1438b97df5c76a803c6199dc5a66be50fed67648
LisTRK <listrk54@gmail.com>
2026-05-27 13:40:52 +0300

    Save latest changes

--- src/sections/Sale/Sale.jsx
+++ src/sections/Sale/Sale.jsx
@@ -1,24 +1,27 @@
-import Button from '@/shared/ui/Button/Button.jsx'
-import './Sale.css'
+import Button from "@/shared/ui/Button/Button.jsx";
+import "./Sale.css";
 
 function Sale({ onSurveyOpen }) {
   return (
     <section className="sale" aria-label="Сонячна електростанція під ключ">
       <div className="sale__inner">
-      <h1 className="sile-header__title">
-        <span className="sile-header__free">FREE</span>
-        <span className="sile-header__solar">SOLAR</span>
-      </h1>
+        <h1 className="sile-header__title">
+          <span className="sile-header__free">FREE</span>
+          <span className="sile-header__solar">SOLAR</span>
+        </h1>
+        <p className="sale__subtitle">вільна енергія для вас</p>
         <h2 className="sale__title">
           Сонце — інвестиція в автономність вашого дому та бізнесу.
         </h2>
         <p className="sale__lead">
-        Переходьте на власну генерацію «під ключ». Отримуйте гарантовану енергонезалежність, захист від блекаутів та економію на десятиліття.
+          Переходьте на власну генерацію «під ключ». Отримуйте гарантовану
+          енергонезалежність, захист від блекаутів та економію на десятиліття по
+          всій тереторії України.
         </p>
         <Button onClick={onSurveyOpen} />
       </div>
     </section>
-  )
+  );
 }
 
-export default Sale
+export default Sale;
